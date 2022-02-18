146ecb3e899f65f8e7d4fac5fc37a146d6dbb1bb
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2022-02-18 22:32:03 +0530

    Added a moving square

--- src/main.ts
+++ src/main.ts
@@ -26,3 +26,22 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 camera.position.z = 5;
 renderer.render(scene, camera);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+}
+
+animate();
+
+/**
+ * 1. Create a scene
+ * 2. Create an object
+ * 3. Create object material
+ * 4. Create a mesh -> bject of a specific material
+ * 5. Add mesh to scene
+ * 6. Position camera
+ * 7. Render scene
+ */
