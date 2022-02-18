6773ce2c5e7dc6df0524720a34d54cb09815ce7b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2022-02-18 22:22:08 +0530

    Added normalized styling Added canvas

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,17 @@
+import './style.css';
+
 import * as THREE from 'three';
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// Renderer to render 3d UI
+const renderer = new THREE.WebGLRenderer();
+// Size of canvas
+// window.innerWidth === innerWidth
+renderer.setSize(innerWidth, innerHeight);
+document.body.appendChild(renderer.domElement);
